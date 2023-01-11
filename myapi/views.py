a2ae04829bce96e8adcd281d7644f772af004dc0
wirapratamaz <wirabagus185@gmail.com>
2023-01-11 12:55:20 +0800

    Create Views Hero

--- myapi/views.py
+++ myapi/views.py
@@ -1,9 +1,11 @@
-#Digunakan untuk mendefinisikan tampilan dari aplikasi
 from django.shortcuts import render
 from .serializers import HeroSerializer
 from .models import Hero
 
 # Create your views here.
+# class that provides a default set of CRUD using ModelViewSet
 class HeroViewSet(viewsets.ModelViewSet):
+    #property will select all the records of Hero table order by 'name'
     queryset = Hero.objects.all().order_by('name')
+    #property will use the HeroSerializer class to convert the model into JSON
     serializer_class = HeroSerializer
