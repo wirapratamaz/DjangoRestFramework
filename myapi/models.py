c76d2ce91df5cf6cd87ed3b348e4e10fc4770309
wirapratamaz <wirabagus185@gmail.com>
2023-01-11 12:36:58 +0800

    migratons model hero

--- myapi/models.py
+++ myapi/models.py
@@ -3,3 +3,12 @@ from django.db import models
 
 
 # Create your models here.
+class Hero(models.Model):
+    # fields on the model
+    name = models.CharField(max_length=60)
+    alias = models.CharField(max_length=60)
+    
+    # method return the string representation of the model
+    def __str__(self):
+        #this method returns the string representation of the primary key
+        return self.name
