0cb3b22978794bda16f2dbe418580b39f8f687bb
wirapratamaz <wirabagus185@gmail.com>
2023-01-11 12:41:08 +0800

    Register Model

--- myapi/admin.py
+++ myapi/admin.py
@@ -1,4 +1,6 @@
 #Digunakan untuk mengelola data pada aplikasi Anda melalui antarmuka web.
 from django.contrib import admin
+from .models import Hero
 
 # Register your models here.
+admin.site.register(Hero)
